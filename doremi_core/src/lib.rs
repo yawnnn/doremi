dde43fed1fbc817bc8c29964703471433e12482d
yawnnn <alessandromy7476@gmail.com>
2026-02-04 21:01:07 +0100

    found small datetime lib

--- doremi_core/src/lib.rs
+++ doremi_core/src/lib.rs
@@ -1,186 +1,89 @@
+use datetime::DateTime;
 use std::{
-    cmp,
     fmt::{self, Debug, Write},
     str::FromStr,
 };
 
 /// [x, y, z]
 #[derive(Debug, PartialEq, Eq)]
-struct RecordList(Vec<String>);
+struct RVec<T>(Vec<T>);
 
-impl fmt::Display for RecordList {
+impl<T: fmt::Display> fmt::Display for RVec<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[{}]", self.0.join(", "))
+        f.write_char('[')?;
+        for (i, elmt) in self.0.iter().enumerate() {
+            if i > 0 {
+                f.write_str(", ")?;
+            }
+            write!(f, "{}", elmt)?
+        }
+        f.write_char(']')
     }
 }
 
-impl FromStr for RecordList {
+impl<T: FromStr> FromStr for RVec<T> {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         s.strip_prefix('[')
             .and_then(|s| s.strip_suffix(']'))
-            .map(|s| {
+            .and_then(|s| {
                 s.split(',')
-                    .map(|s| s.trim().to_owned())
-                    .collect::<Vec<_>>()
+                    .map(|s| s.trim().parse().ok())
+                    .collect::<Option<Vec<_>>>()
             })
-            .map(|v| RecordList(v))
+            .map(|v| RVec(v))
             .ok_or(())
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Default, Clone, Copy)]
-struct Date {
-    year: u16,
-    month: u8,
-    day: u8,
-}
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct RDateTime(DateTime);
 
-impl Date {
-    fn new(year: u16, month: u8, day: u8) -> Self {
-        Date { year, month, day }
+impl RDateTime {
+    const FMT: &str = "%Y-%m-%d %H:%M:%S";
+
+    fn inner(self) -> DateTime {
+        self.0
     }
 }
 
-impl fmt::Display for Date {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:04}-{:02}-{:02}", self.year, self.month, self.day)
-    }
-}
-
-impl FromStr for Date {
+impl FromStr for RDateTime {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut it = s.split('-');
-        Ok(Date {
-            year: it.next().ok_or(())?.parse().map_err(|_| ())?,
-            month: it.next().ok_or(())?.parse().map_err(|_| ())?,
-            day: it.next().ok_or(())?.parse().map_err(|_| ())?,
-        })
+        Ok(RDateTime(DateTime::parse(s, Self::FMT).map_err(|_| ())?))
     }
 }
 
-impl Ord for Date {
-    fn cmp(&self, other: &Self) -> cmp::Ordering {
-        self.year
-            .cmp(&other.year)
-            .then(self.month.cmp(&other.month))
-            .then(self.day.cmp(&other.day))
-    }
-}
-
-impl PartialOrd for Date {
-    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Default, Clone, Copy)]
-struct Time {
-    hour: u8,
-    minute: u8,
-    second: u8,
-}
-
-impl Time {
-    fn new(hour: u8, minute: u8, second: u8) -> Self {
-        Time {
-            hour,
-            minute,
-            second,
-        }
-    }
-}
-
-impl fmt::Display for Time {
+impl fmt::Display for RDateTime {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}:{:02}", self.hour, self.minute, self.second)
+        fmt::Display::fmt(&self.0.format(Self::FMT), f)
     }
 }
 
-impl FromStr for Time {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut it = s.split(':').flat_map(|s| s.parse::<u8>().ok());
-        Ok(Time {
-            hour: it.next().ok_or(())?,
-            minute: it.next().ok_or(())?,
-            second: it.next().ok_or(())?,
-        })
-    }
-}
-
-impl Ord for Time {
-    fn cmp(&self, other: &Self) -> cmp::Ordering {
-        self.hour
-            .cmp(&other.hour)
-            .then(self.minute.cmp(&other.minute))
-            .then(self.second.cmp(&other.second))
-    }
-}
-
-impl PartialOrd for Time {
-    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-/// 2026-01-27 10:22:01
-#[derive(Debug, PartialEq, Eq, Default, Clone, Copy)]
-struct DateTime {
-    date: Date,
-    time: Time,
-}
-
-impl fmt::Display for DateTime {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {}", self.date, self.time)
-    }
-}
-
-impl FromStr for DateTime {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.split_once(' ')
-            .and_then(|(date, time)| {
-                Some(DateTime {
-                    date: date.parse().ok()?,
-                    time: time.parse().ok()?,
-                })
-            })
-            .ok_or(())
-    }
-}
-
-impl Ord for DateTime {
-    fn cmp(&self, other: &Self) -> cmp::Ordering {
-        self.date.cmp(&other.date).then(self.time.cmp(&other.time))
-    }
-}
-
-impl PartialOrd for DateTime {
-    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        Some(self.cmp(other))
+impl From<DateTime> for RDateTime {
+    fn from(value: DateTime) -> Self {
+        RDateTime(value)
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct Record {
     id: u32,
-    ctime: DateTime,
-    utime: DateTime,
-    tags: RecordList,
+    ctime: RDateTime,
+    utime: RDateTime,
+    tags: RVec<String>,
     name: String,
     data: String,
 }
 
 impl Record {
+    pub const SEP: &str = "\n---\n";
+
     const K_ID: &str = "id";
     const K_CTIME: &str = "ctime"; // creation time
     const K_UTIME: &str = "utime"; // update time
     const K_TAGS: &str = "tags";
     const K_NAME: &str = "name";
-    pub const SEP: &str = "\n---\n";
 }
 
 impl fmt::Display for Record {
@@ -208,7 +111,7 @@ impl FromStr for Record {
 
         let id = parse_kv(it.next().ok_or(())?, Self::K_ID)?;
         let ctime = parse_kv(it.next().ok_or(())?, Self::K_CTIME)?;
-        let utime = parse_kv(it.next().ok_or(())?, Self::K_UTIME).unwrap_or_default();
+        let utime = parse_kv(it.next().ok_or(())?, Self::K_UTIME).unwrap_or(ctime);
         let tags = parse_kv(it.next().ok_or(())?, Self::K_TAGS)?;
         let name = parse_kv(it.next().ok_or(())?, Self::K_NAME)?;
         let data = it.map(str::trim).collect::<Vec<_>>().join("\n"); // ignore only trailing endl
@@ -250,45 +153,31 @@ fn insert<W: std::fmt::Write + ?Sized>(w: &mut W, records: &[Record]) -> std::fm
 #[cfg(test)]
 mod tests {
     use super::*;
+    use datetime::datetime;
 
     #[test]
     fn to_string() {
         let mut buf = String::new();
 
-        let ctime = DateTime {
-            date: Date::new(2026, 01, 27),
-            time: Time::new(10, 22, 01),
-        };
-        let utime = ctime;
+        let ctime = datetime!(2026-01-27 10:22:01).into();
+        let utime = datetime!(2026-01-27 10:22:01).into();
         let r = Record {
             id: 0,
             ctime: ctime,
             utime: utime,
-            tags: RecordList(vec!["tag1".to_string(), "tag2".to_string()]),
+            tags: RVec(vec!["tag1".to_string(), "tag2".to_string()]),
             name: "note 1".to_string(),
             data: "multiline\ndata".to_string(),
         };
         insert(&mut buf, &[r]).unwrap();
 
-        let ctime = DateTime {
-            time: Time {
-                second: ctime.time.second + 1,
-                ..ctime.time
-            },
-            ..ctime
-        };
-        let utime = DateTime {
-            time: Time {
-                hour: 20,
-                ..ctime.time
-            },
-            ..ctime
-        };
+        let ctime = datetime!(2026-01-27 10:22:02).into();
+        let utime = datetime!(2026-01-27 20:22:02).into();
         let r = Record {
             id: 1,
             ctime: ctime,
             utime: utime,
-            tags: RecordList(vec!["tag1".to_string(), "tag2".to_string()]),
+            tags: RVec(vec!["tag1".to_string(), "tag2".to_string()]),
             name: "note 1".to_string(),
             data: "one-line data".to_string(),
         };
@@ -339,45 +228,30 @@ one-line data
 
         let mut it = select(s.as_bytes());
 
-        let ctime = DateTime {
-            date: Date::new(2026, 01, 27),
-            time: Time::new(10, 22, 01),
-        };
-        let utime = ctime;
+        let ctime = datetime!(2026-01-27 10:22:01).into();
+        let utime = datetime!(2026-01-27 10:22:01).into();
 
         assert_eq!(
             Some(Record {
                 id: 0,
                 ctime: ctime,
                 utime: utime,
-                tags: RecordList(vec!["tag1".to_string(), "tag2".to_string()]),
+                tags: RVec(vec!["tag1".to_string(), "tag2".to_string()]),
                 name: "note 1".to_string(),
                 data: "multiline\ndata".to_string(),
             }),
             it.next()
         );
 
-        let ctime = DateTime {
-            time: Time {
-                second: ctime.time.second + 1,
-                ..ctime.time
-            },
-            ..ctime
-        };
-        let utime = DateTime {
-            time: Time {
-                hour: 20,
-                ..ctime.time
-            },
-            ..ctime
-        };
+        let ctime = datetime!(2026-01-27 10:22:02).into();
+        let utime = datetime!(2026-01-27 20:22:02).into();
 
         assert_eq!(
             Some(Record {
                 id: 1,
                 ctime: ctime,
                 utime: utime,
-                tags: RecordList(vec!["tag1".to_string(), "tag2".to_string()]),
+                tags: RVec(vec!["tag1".to_string(), "tag2".to_string()]),
                 name: "note 1".to_string(),
                 data: "one-line data".to_string(),
             }),
